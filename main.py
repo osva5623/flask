c5277e41833d49e52716c4f614505d00d2e08d5c
Oswaldo Jimenez <oswaldo.jimenez5623@gmail.com>
2022-02-17 11:44:05 -0600

    adicion de wtf

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from flask import Flask,request,make_response,redirect,render_template,session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired
+
+
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
 todos=['1','2','3']
 app.config['SECRET_KEY']='SUPER SECRETO'
+
+class LoginForm(FlaskForm):
+    user_name=StringField('Nombre de usuario',validators=[DataRequired()])
+    password=PasswordField('Contraseña', validators=[DataRequired()])
+    submit=SubmitField('Enviar')
+
 @app.errorhandler(404)
 def not_found(error):
     
@@ -20,9 +31,11 @@ def index():
 @app.route('/welcome')
 def hello():
     user_ip=session.get('user_ip')
+    login_form=LoginForm()
     contexto={
         'user_ip':user_ip,
-        'todos':todos
+        'todos':todos,
+        'login_form':login_form
     }
     return render_template('ip_html.html',**contexto)
     # return f'Hellow world, platzS tu ip es {format(user_ip)}'
