247daee22cda44b8eb2a42e737ac52b157444137
Oswaldo Jimenez <oswaldo.jimenez5623@gmail.com>
2022-02-21 08:42:01 -0600

    flashes

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask,request,make_response,redirect,render_template,session
+from flask import Flask,request,make_response,redirect,render_template,session,url_for,flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired
-
 
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
@@ -28,14 +27,22 @@ def index():
     return response
     
 
-@app.route('/welcome')
+@app.route('/welcome', methods=['GET', 'POST'])
 def hello():
     user_ip=session.get('user_ip')
     login_form=LoginForm()
+    user_name=session.get('user_name')
     contexto={
         'user_ip':user_ip,
         'todos':todos,
-        'login_form':login_form
+        'login_form':login_form,
+        'user_name':user_name
     }
+    if login_form.validate_on_submit():
+        user_name= login_form.user_name.data
+        session['user_name']=user_name
+        flash('usuario creado')
+        return redirect(url_for('index'))
+
     return render_template('ip_html.html',**contexto)
     # return f'Hellow world, platzS tu ip es {format(user_ip)}'
